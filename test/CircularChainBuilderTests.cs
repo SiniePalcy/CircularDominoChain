89fb635761655acd9c1003937d84dc3800fae71e
sergei.filippov <72672477+SiniePalcy@users.noreply.github.com>
2025-01-07 16:33:52 +0100

    Added Test project

---
+++ test/CircularChainBuilderTests.cs
@@ -0,0 +1,29 @@
+﻿public class CircularChainBuilderTests
+{
+    [Test]
+    public void IsCanBeCircular_Success()
+    {
+        var testCase = new[]
+        {
+            new Domino(DominoSide.Two, DominoSide.One),
+            new Domino(DominoSide.Two, DominoSide.Three),
+            new Domino(DominoSide.Three, DominoSide.One),
+        };
+
+        Assert.That(CircularChainBuilder.IsCanBeCircular(testCase), Is.True);
+    }
+
+    [Test]
+    public void IsCanBeCircular_WithDoubledDominos_Failed()
+    {
+        var testCase = new[]
+        {
+            new Domino(DominoSide.Two, DominoSide.One),
+            new Domino(DominoSide.Two, DominoSide.Four),
+            new Domino(DominoSide.Four, DominoSide.One),
+            new Domino(DominoSide.Three, DominoSide.Three),
+        };
+
+        Assert.That(CircularChainBuilder.IsCanBeCircular(testCase), Is.False);
+    }
+}
