0fd436b9ec33dc48ae2d25a020ccaae6a3a9a715
sergei.filippov <72672477+SiniePalcy@users.noreply.github.com>
2025-01-07 15:35:21 +0100

    Circular check added

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,46 @@
 ﻿var pack = new DominosPack();
-pack.OutputDominos();
 
-var takedDominos = pack.TakeRandomDominos(5);
+
+Console.Write("How many dominos you'll select: ");
+var selectedDominosCount = int.Parse(Console.ReadLine());
+
+var takedDominos = pack.TakeRandomDominos(selectedDominosCount);
+Console.WriteLine("You selected set: ");
 takedDominos.OutputDominos();
 
+if (!IsCanBeCircular(takedDominos))
+{
+    Console.WriteLine("Your set can't be circular chain");
+    return;
+}
+
+
+static bool IsCanBeCircular(IEnumerable<Domino> dominosSet)
+{
+    var values = new Dictionary<DominoSide, int>();
+    foreach (var domino in dominosSet)
+    {
+        if (!values.ContainsKey(domino.SideA))
+        {
+            values.Add(domino.SideA, 0);
+        }
+
+        if (!values.ContainsKey(domino.SideB))
+        {
+            values.Add(domino.SideB, 0);
+        }
+
+        values[domino.SideA]++;
+        values[domino.SideB]++;
+    }
+
+    foreach (var side in values.Keys)
+    {
+        if (values[side] % 2 != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
